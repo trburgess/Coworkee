8173ed4e5580ccdc82c4cd5addb79cf4290ca5c1
Tom Burgess <tom.burgess@irmsecurity.com>
2017-11-17 16:59:42 +0000

    Update `session` to compare the hash after retrieving a user.

--- server/utils/session.js
+++ server/utils/session.js
@@ -7,34 +7,40 @@
 var errors = require('./errors');
 var config = require('./config');
 var models = require('../models');
-var jwt = require('jsonwebtoken');
+var jwt    = require('jsonwebtoken');
+var helpers = require('./helpers.js');
 
 module.exports = {
 
     readonly: config.session.readonly,
 
-    initiate: function(username, password, res) {
-        return models.Person.scope('nested').findOne({
+    initiate: function (username, password, res) {
+        return models.Person.scope('auth').findOne({
             where: {
-                password: password,
                 $or: [
-                    { username: username },
-                    { email: username }
+                    {username: username},
+                    {email: username}
                 ]
             }
-        }).then(function(user) {
-            if (!user) {
+        }).then(function (authUser) {
+            if (!authUser || !helpers.hashCompareSync(password, authUser.password)) {
                 throw errors.types.invalidParams({
                     path: 'username', message: 'Invalid username and/or password'
                 });
             }
 
+            return models.Person.scope('nested').findOne({
+                where: {
+                    id: authUser.id
+                }
+            });
+        }).then(function (user) {
             var duration = config.session.duration;
-            var expires = new Date(Date.now() + duration*1000);
-            var token = jwt.sign(
-                { user_id: user.get('id') },
+            var expires  = new Date(Date.now() + duration * 1000);
+            var token    = jwt.sign(
+                {user_id: user.get('id')},
                 config.session.secret,
-                { expiresIn: duration });
+                {expiresIn: duration});
 
             return {
                 user: user,
@@ -44,18 +50,18 @@ module.exports = {
         });
     },
 
-    verify: function(request) {
-        return new Promise(function(resolve, reject) {
+    verify: function (request) {
+        return new Promise(function (resolve, reject) {
             // https://jwt.io/introduction/#how-do-json-web-tokens-work-
-            var header = request.headers && request.headers.authorization;
-            var matches = header? /^Bearer (\S+)$/.exec(header) : null;
-            var token = matches && matches[1];
+            var header  = request.headers && request.headers.authorization;
+            var matches = header ? /^Bearer (\S+)$/.exec(header) : null;
+            var token   = matches && matches[1];
 
             if (!token) {
-                 return reject(errors.types.unauthorized('No authorization token was found'));
+                return reject(errors.types.unauthorized('No authorization token was found'));
             }
 
-            jwt.verify(token, config.session.secret, function(err, decoded) {
+            jwt.verify(token, config.session.secret, function (err, decoded) {
                 if (err) {
                     return reject(errors.fromJwtError(err));
                 }
@@ -64,7 +70,7 @@ module.exports = {
                     where: {
                         id: decoded.user_id
                     }
-                }).then(function(user) {
+                }).then(function (user) {
                     if (!user) {
                         throw errors.types.authTokenInvalid();
                     }
@@ -74,7 +80,7 @@ module.exports = {
                         token: token,
                         expires: new Date(decoded.exp)
                     });
-                }).catch(function(err) {
+                }).catch(function (err) {
                     reject(err);
                 });
             });
